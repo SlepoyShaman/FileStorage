1636046e93015303fd7837c8730716f92eefa959
SlepoyShaman <ilushin905@gmail.com>
2025-11-03 15:03:31 +0300

    add App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+import { onMounted } from "vue";
+import { mutations } from "@/store"; // Import your store's mutations
+mutations.setLoading("main-app", true);
+export default {
+  name: "app",
+  computed: {},
+  setup() {
+    onMounted(() => {
+      mutations.setLoading("main-app", false);
+      // Query the loading element and remove it from the DOM
+      const loadingDiv = document.getElementById("loading");
+      if (loadingDiv) {
+        loadingDiv.remove();
+      }
+    });
+  },
+};
+</script>
+
+<style>
+/* Always load styles.css */
+@import "./css/styles.css";
+@import "./css/dark.css";
+</style>
